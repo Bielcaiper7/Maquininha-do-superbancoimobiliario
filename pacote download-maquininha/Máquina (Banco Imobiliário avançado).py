70822b029861c738877abf3edf1b706fca9e442e
Gabriel Caiper <bielcaiper07@gmail.com>
2022-08-05 09:22:14 -0300

    commit full little machine

---
+++ pacote download-maquininha/Máquina (Banco Imobiliário avançado).py
@@ -0,0 +1,34 @@
+from função_máquina_avançada import *
+propriedades = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+cartões = []
+msg('Máquina', 6)
+msg('Registro de cartões', 3)
+lercartão(cartões)
+msg('Cartões cadastrados', 2)
+for c in cartões:
+    print(f'{c[0]:<28}R${c[1]:.2f}')
+print('-=' * 19)
+while True:
+    while True:
+        try:
+            escolha = int(input('''[0] Receber do banco
+[1] Pagar ao banco
+[2] Transferir dinheiro para jogador
+escolha: '''))
+        except:
+            print('\033[1;31mERRO:Digite um número válido\033[m')
+        else:
+            break
+        finally:
+            print()
+    if escolha == 0:
+        recban(cartões)
+        print('-=' * 19)
+    elif escolha == 1:
+        pagban(cartões)
+        print('-=' * 19)
+    elif escolha == 2:
+        trandin(cartões)
+        print('-=' * 19)
+    else:
+        print('\033[1;31mERRO:Digite um número válido !\033[m')
